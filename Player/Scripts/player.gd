b8dbc5d157ecfb6705dbf1e41d5962aef045c3ba
jwong65 <jason.wong6507@gmail.com>
2025-05-26 06:11:00 -0400

    Direction normalized for character, Motion mode changed to floating

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -19,7 +19,7 @@ func _process(delta):
 	
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
+	direction = direction.normalized()
 	pass
 
 func _physics_process(delta):
