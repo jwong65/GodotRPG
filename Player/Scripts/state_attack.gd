3a5a56b8e8b7a44ae3344ec38976fe057bd629f4
jwong65 <jason.wong6507@gmail.com>
2025-05-27 12:31:16 -0400

    Added sound effects to the state attack

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -1,10 +1,13 @@
 class_name State_Attack extends State
+
+@export var attack_sound : AudioStream
+
 @onready var walk: State = $"../Walk"
 @onready var attack: State = $"."
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 @onready var attack_anim: AnimationPlayer = $"../../Attack/AttackSprite01/AnimationPlayer"
 @onready var idle: State = $"../Idle"
-
+@onready var audio: AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
 
 var attacking : bool = false
 
@@ -13,6 +16,10 @@ func Enter()-> void:
 	player.UpdateAnimation('attack')
 	attack_anim.play("attack_" + player.AnimationDirection())
 	animation_player.animation_finished.connect(EndAttack)
+	
+	audio.stream = attack_sound
+	audio.pitch_scale = randf_range(0.9, 1.1)
+	audio.play()
 	attacking = true
 	pass
 # Function for when player exits the state
